623b2ac1295345ade92b8caf8fb73a15b7b078b3
Sprocket <Sprocket@10.76.17.2>
2018-02-18 11:39:08 -0800

    updated talon numbers

--- src/org/usfirst/frc/team3473/robot/RobotMap.java
+++ src/org/usfirst/frc/team3473/robot/RobotMap.java
@@ -20,19 +20,19 @@ import edu.wpi.first.wpilibj.Solenoid;
  */
 public class RobotMap {
 	// Drivetrain
-	public static WPI_TalonSRX frontLeft = new WPI_TalonSRX(-1);
-	public static WPI_TalonSRX backLeft = new WPI_TalonSRX(-1);
-	public static WPI_TalonSRX frontRight = new WPI_TalonSRX(-1);
-	public static WPI_TalonSRX backRight = new WPI_TalonSRX(-1);
-	public static DoubleSolenoid gearPneumatics = new DoubleSolenoid(-1, -1);
+	public static WPI_TalonSRX frontLeft = new WPI_TalonSRX(6);
+	public static WPI_TalonSRX backLeft = new WPI_TalonSRX(7);
+	public static WPI_TalonSRX frontRight = new WPI_TalonSRX(5);
+	public static WPI_TalonSRX backRight = new WPI_TalonSRX(4);
+	public static DoubleSolenoid gearPneumatics = new DoubleSolenoid(0, 1);
 
 	// Intake
-	public static WPI_TalonSRX intakeElevator = new WPI_TalonSRX(-1);
-	public static WPI_TalonSRX intakeLeft = new WPI_TalonSRX(-1);
-	public static WPI_TalonSRX intakeRight = new WPI_TalonSRX(-1);
-	public static Solenoid actuationPiston = new Solenoid(-1);
+	public static WPI_TalonSRX intakeElevator = new WPI_TalonSRX(10);
+	public static WPI_TalonSRX intakeLeft = new WPI_TalonSRX(1);
+	public static WPI_TalonSRX intakeRight = new WPI_TalonSRX(2);
+	public static Solenoid actuationPiston = new Solenoid(2);
 
 	// Climb
-	public static WPI_TalonSRX climbElevator1 = new WPI_TalonSRX(-1);
-	public static WPI_TalonSRX climbElevator2 = new WPI_TalonSRX(-1);
+	public static WPI_TalonSRX climbElevator1 = new WPI_TalonSRX(3);
+	public static WPI_TalonSRX climbElevator2 = new WPI_TalonSRX(8);
 }
