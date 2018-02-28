f373fef13daf246c03b1e2c3b55ed295a81494e0
qompute <qhchen09@gmail.com>
2018-02-27 17:09:00 -0800

    minor changes to Drive class

--- src/org/usfirst/frc/team3473/robot/commands/Drive.java
+++ src/org/usfirst/frc/team3473/robot/commands/Drive.java
@@ -11,16 +11,12 @@ import edu.wpi.first.wpilibj.command.Command;
 public class Drive extends Command {
 
 	public Drive() {
-		// Use requires() here to declare subsystem dependencies
-		// eg. requires(chassis);
 		requires(Robot.drivetrain);
 	}
 
-	// Called just before this Command runs the first time
 	protected void initialize() {
 	}
 
-	// Called repeatedly when this Command is scheduled to run
 	protected void execute() {
 		double speed = -OI.leftJoystick.getY() * .8;
 		double turn = OI.rightJoystick.getX();
@@ -31,17 +27,15 @@ public class Drive extends Command {
 		Robot.drivetrain.arcadeDrive(speed, turn);
 	}
 
-	// Make this return true when this Command no longer needs to run execute()
 	protected boolean isFinished() {
 		return false;
 	}
 
-	// Called once after isFinished returns true
 	protected void end() {
+		Robot.drivetrain.arcadeDrive(0.0, 0.0);
 	}
 
-	// Called when another command which requires one or more of the same
-	// subsystems is scheduled to run
 	protected void interrupted() {
+		end();
 	}
 }
