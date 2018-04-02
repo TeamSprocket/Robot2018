658e7d78374645635a39baa1c96db35f25552ebb
Catherine <2111catlu@gmail.com>
2018-04-02 12:02:56 -0700

    added StayStraight commands

---
+++ src/org/usfirst/frc/team3473/robot/commands/StayStraightGyro.java
@@ -0,0 +1,65 @@
+package org.usfirst.frc.team3473.robot.commands;
+
+import org.usfirst.frc.team3473.robot.Robot;
+import org.usfirst.frc.team3473.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class StayStraightGyro extends Command {
+	private static final double DEFAULT_SPEED = 0.5;
+	private double targetDistance;
+	private double rightSpeed;
+	private double leftSpeed;
+	private double kP = 0.03;
+	
+	public StayStraightGyro(double distance) {
+		this(Math.signum(distance) * DEFAULT_SPEED, distance);
+	}
+
+	public StayStraightGyro(double speed, double distance) {
+		requires(Robot.drivetrain);
+		rightSpeed = speed;
+		leftSpeed = speed;
+		targetDistance = Math.abs(distance);
+	}
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    		RobotMap.gyro.reset();
+    		RobotMap.leftEncoder.reset();
+    		RobotMap.rightEncoder.reset();
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {	
+    		double currentAngle = RobotMap.gyro.getAngle();
+    		rightSpeed += (kP * currentAngle);
+		leftSpeed -= (kP * currentAngle);
+//    		if(currentAngle > 0) {
+//    			rightSpeed += (kP * currentAngle);
+//    		}
+//    		else if(currentAngle < 0) {
+//    			leftSpeed -= (kP * currentAngle);
+//    		}
+    		Robot.drivetrain.tankDrive(leftSpeed, rightSpeed);
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+    	double averageDistance = Math.abs(RobotMap.leftEncoder.getDistance()
+				+ RobotMap.rightEncoder.getDistance()) / 2.0;
+		return averageDistance >= targetDistance;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
